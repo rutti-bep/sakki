909171c1a836d77c94cf13eeda93cc16292208b8
rutti-bep <konnosatoru@konnoakira-no-MacBook.local>
2017-01-23 17:00:02 +0900

    WIP

--- models/entry.rb
+++ models/entry.rb
@@ -1,6 +1,6 @@
 class Entry
-    attr_accessor :title
-    attr_accessor :body
-    attr_accessor :posted_at
-    attr_accessor :publish
+  COLUMNS = [:id, :title, :body, :posted_at, :published]
+  COLUMNS.each do |column|
+     attr_accessor column
+  end
 end
--- repositories/entry_repository.rb
+++ repositories/entry_repository.rb
@@ -1,15 +1,51 @@
 class EntryRepository
-  def initialize
-      @entries = []
+  include Enumerable
+  def initialize(db)
+      @db = db
   end
 
   def save(entry)
-    @entries.push(entry)
-    return @entries.length - 1
+    columns = Entry::COLUMNS.reject { |key| key == :id }
+    values = columns.map { |key| entry.instance_variable_get("@#{key}") }
+    query = "INSERT INTO `entries` (#{columns.join(", ")}) VALUES (#{columns.map { '?' }.join(', ')})"
+    stmt = @db.prepare(query)
+    stmt.execute(*values)
+    entry.id = @db.last_id
+    return entry.id
   end
 
   def fetch(id)
-    @entries[id]
-      # なんか取得
+    query = "SELECT * FROM `entries` WHERE `id` = ?"
+    stmt = @db.prepare(query)
+    res = stmt.execute(id)
+
+    data = res.first
+    entry = Entry.new(data)
+    entry.title = data["title"]
+    entry.body = data["body"]
+
+    return entry
   end
+
+  def each(&block)
+    entries = []
+    query = "SELECT * FROM `entries`"
+    res = @db.query(query)
+
+    res.each do |row|
+        entry = Entry.new(row)
+        entries.push(entry)
+    end
+
+    entries.each(&block)
+  end
+
+  def initialize(attrs = {})
+    attrs.each_pair do |key, val|
+      key = key.to_s.to_sym
+      if COLUMNS.include?(key)
+        instance_variable_set("@#{key}", val)
+      end
+    end
+  end  
 end
