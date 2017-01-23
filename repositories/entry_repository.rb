035f1f066a342821a80c6fc1ecead5da63fb088c
rutti-bep <konnosatoru@konnoakira-no-MacBook.local>
2017-01-23 14:45:55 +0900

    add 投稿機能

---
+++ repositories/entry_repository.rb
@@ -0,0 +1,15 @@
+class EntryRepository
+  def initialize
+      @entries = []
+  end
+
+  def save(entry)
+    @entries.push(entry)
+    return @entries.length - 1
+  end
+
+  def fetch(id)
+    @entries[id]
+      # なんか取得
+  end
+end
