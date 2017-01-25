bffeba8e8632be75e5b67fd802614ae05ab8b0e1
rutti-bep <konnosatoru@konnoakira-no-MacBook.local>
2017-01-25 15:23:37 +0900

    ready for improvements

--- gulpfile.js
+++ gulpfile.js
@@ -7,30 +7,21 @@ const process = require('gulp-process');
 
 gulp.task('sass', function () {
   return gulp.src('./sass/**/*.scss')
+    .pipe(sourcemaps.init())
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./tmp/css'));
+    .pipe(postcss([ autoprefixer() ]))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('./public/css'));
 });
 
 gulp.task('sass:watch', function () {
   gulp.watch('./sass/**/*.scss', ['sass']);
 });
 
-gulp.task('autoprefixer', ['sass'], function () {
-    return gulp.src('./tmp/css/*.css')
-        .pipe(sourcemaps.init())
-        .pipe(postcss([ autoprefixer() ]))
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest('./public/css'));
-});
 
-gulp.task('autoprefixer:watch', function () {
-  gulp.watch('./tmp/css/*.css', ['autoprefixer']);
-});
-
-gulp.task('build', ['sass', 'autoprefixer']);
+gulp.task('build', ['sass']);
 
 gulp.task('default', ['build'], function() {
   gulp.watch('./sass/**/*.scss', ['sass']);
-  gulp.watch('./tmp/css/*.css', ['autoprefixer']);
   process.start('guard', 'bundle', ['exec', 'guard', '--no-interactions']);
 });
