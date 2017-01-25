35c9383ae4ba9cc836ae77f0d573908cac707cbd
rutti-bep <konnosatoru@konnoakira-no-MacBook.local>
2017-01-25 16:11:38 +0900

    add basic認証

--- app.rb
+++ app.rb
@@ -10,47 +10,71 @@ Dir["repositories/*.rb"].each do |model|
 end
 
 class App < Sinatra::Base
-    configure :development do
-        register Sinatra::Reloader
-    end
+  configure :development do
+    register Sinatra::Reloader
+  end
 
-    configure do
-        set :views,settings.root + "/views"
-    end
+  configure do
+    set :views,settings.root + "/views"
+  end
 
-    def self.database_config
-        YAML.load_file("config/database.yml")[ENV['RACK_ENV'] || 'development']
-    end
+  def self.database_config
+    YAML.load_file("config/database.yml")[ENV['RACK_ENV'] || 'development']
+  end
 
-    def self.database
-        @database ||= Mysql2::Client.new(database_config)
-    end
-    helpers do
-      def entry_repository
-        @@entry_repository ||= EntryRepository.new(App.database)
+  def self.database
+    @database ||= Mysql2::Client.new(database_config)
+  end
+
+  helpers do
+    def protected!
+      unless authorized?
+        response['WWW-Authenticate'] = %(Basic realm="Restricted Area")
+        throw(:halt,[401,"Not authorized\n"])
       end
     end
 
-    get '/' do
-        slim :index
+    def authorized?
+      @auth ||= Rack::Auth::Basic::Request.new(request.env)
+      @auth.provided? && @auth.basic? && @auth.credentials &&
+      @auth.credentials == [ENV['BLOG_USERNAME'],ENV['BLOG_PASSWORD']]
     end
 
-    get "/entries/new" do
-        slim :new
+    def entry_repository
+      @@entry_repository ||= EntryRepository.new(App.database)
     end
 
-    post "/entries" do
-      entry = Entry.new
-      entry.title = params[:title]
-      entry.body = params[:body]
-      id = entry_repository.save(entry)
-
-      redirect to("/entries/#{id}")
+    def title
+      str = ""
+      if @entry
+        str = @entry.title + " - "
+      end
+      str + "ruttiのブログ"
     end
+  end
 
-    get "/entries/:id" do
-      @entry = entry_repository.fetch(params[:id].to_i)
+  get '/' do
+    slim :index
+  end
 
-      slim :entry
-    end
+  get "/entries/new" do
+    protected!
+    slim :new
+  end
+
+  post "/entries" do
+    protected!
+    entry = Entry.new
+    entry.title = params[:title]
+    entry.body = params[:body]
+    id = entry_repository.save(entry)
+
+    redirect to("/entries/#{id}")
+  end
+
+  get "/entries/:id" do
+    @entry = entry_repository.fetch(params[:id].to_i)
+
+    slim :entry
+  end
 end
