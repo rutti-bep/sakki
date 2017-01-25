f6730dc063af76f951161706db03f6fbfbd05e8b
rutti-bep <konnosatoru@konnoakira-no-MacBook.local>
2017-01-25 14:06:59 +0900

    WIP

---
+++ models/markdown_processer/mention_filter.rb
@@ -0,0 +1,16 @@
+class MarkdownProcessor
+    class MentionFilter < HTML::Pipeline::Filter
+        def call
+            doc.search('.//text()').each do |node|
+            html = node.to_heml.gsub(/@[a-zA-Z0-9_]+/) do |mention|
+                twitterize(mention[1..-1])
+            end
+            node.replace(html)
+        end
+        doc
+    end
+
+    def twitterize(user)
+        %Q{<a href="https://twitter.com/#{user}">@#{user}</a>}
+    end
+end
